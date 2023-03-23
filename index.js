dd491be38433c201a0d6817f6c0dab77fd51f337
Austin Mazur <mazuraustin1@gmail.com>
2023-03-23 21:32:02 +0100

    Finish iterations 1 and 2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
   get(pos) {}
 
@@ -15,3 +22,5 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+console.log(SortedList);
