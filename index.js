d5e2677222050888596443b468624fa38bf6cdd1
Austin Mazur <mazuraustin1@gmail.com>
2023-03-24 18:57:06 +0100

    Done

--- index.js
+++ index.js
@@ -6,19 +6,59 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort((a, b) => a - b)
-    this.length = this.items.length
+    this.length = this.items.length;
+    return this.items.sort((a, b) => a - b);
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items.at(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    //if nothing in the array, throws the error
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      //Returns the highest out of whats in the array
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    //same concept as the highest number, just now using the Math.min()
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
 
-  avg() {}
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      this.items.forEach((el) => {
+        sum += el;
+      });
+    }
+    return sum;
+  }
+
+  avg() {
+   
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      //takes the sum from previous method 
+       return this.sum() / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
